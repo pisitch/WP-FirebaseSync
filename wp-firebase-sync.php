b687997365ab5ffd8d6cde385d085304fcff33a0
GodSpeed <puksigon@gmail.com>
2019-01-20 20:15:45 +0700

    Modify plugin discription

--- wp-firebase-sync.php
+++ wp-firebase-sync.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: WP Firebase Sync
-Plugin URI: https://github.com/jaredchu/WP-FirebaseSync
+Plugin URI: https://github.com/pisitch/WP-FirebaseSync
 Description: Plugin that allow WordPress to sync Post data with Firebase in real time.
-Version: 1.0
-Author: Jared Chu
-Author URI: http://jaredchu.com/
+Version: 1.1
+Author: Bawornsit C. 
+Original Author: Jared Chu
 License: MIT
 */
 
